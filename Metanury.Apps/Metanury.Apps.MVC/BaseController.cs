4c118225ef461bcaec3feb73d81cf9f94cb56fff
Park Hee sung <yunlang97@gmail.com>
2022-02-23 14:50:48 +0900

    MVC 도우미 프로젝트 추가

---
+++ Metanury.Apps/Metanury.Apps.MVC/BaseController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using System;
+
+namespace Metanury.Apps.MVC
+{
+    public class BaseController : Controller
+    {
+        protected IActionContextAccessor Accessor { get; set; }
+
+        public BaseController(IActionContextAccessor accessor) : base()
+        {
+            this.Accessor = accessor;
+        }
+
+        protected virtual string CookieGet(string key)
+        {
+            return this.Accessor.ActionContext.HttpContext.Request.Cookies[key];
+        }
+
+        protected virtual void CookieSet(string key, string value, DateTime? expireTime = null)
+        {
+            CookieOptions option = new CookieOptions();
+            if (expireTime.HasValue)
+            {
+                option.Expires = expireTime.Value;
+            }
+
+            this.Accessor.ActionContext.HttpContext.Response.Cookies.Append(key, value, option);
+        }
+
+        protected virtual void CookieErase(string key)
+        {
+            this.Accessor.ActionContext.HttpContext.Response.Cookies.Delete(key);
+        }
+
+        public HtmlString WriteContent(string content, bool IsEnter = false)
+        {
+            if (IsEnter)
+            {
+                content = content.Replace("\r\n", "<br/>");
+                content = content.Replace("\r", "<br/>");
+                content = content.Replace("\n", "<br/>");
+                content = content.Replace(Environment.NewLine, "<br/>");
+            }
+
+            return new HtmlString(content);
+        }
+    }
+}
